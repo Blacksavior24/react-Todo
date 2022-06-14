bbfaaee35d50688e9177c4f2c54b1fca670773d4
Emerson Villalta <reaperfocs@gmail.com>
2022-06-13 22:46:13 -0500

    sintaxis con todoContex

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import './TodoCounter.css';
+import {TodoContext} from '../TodoContext'
 
-function TodoCounter(props){
+function TodoCounter(){
+    const {totalTodos, completedTodos} = React.useContext(TodoContext);
     return(
-        <h2 className="TodoCounter"> Has complentado {props.completados} de {props.total} ToDos</h2>
+        <h2 className="TodoCounter"> Has complentado {completedTodos} de {totalTodos} ToDos</h2>
     )
 }
 
